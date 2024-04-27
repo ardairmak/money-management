deefecc0818a6d097408c7578da71e5afe810941
muberragul <hmuberragul@gmail.com>
2024-04-27 22:43:38 +0300

    login sayfasının açılması düzeltildi

--- components/MainComponent.tsx
+++ components/MainComponent.tsx
@@ -3,13 +3,20 @@ import { Fragment } from 'react'
 import Navigation from '../navigation/Navigation'
 import FloatingActionButton from './FloatingActionButton'
 import HamburgerBar from './HamburgerBar'
+import { NavigationProp } from '../type';
 
-export default function MainComponent() {
+interface CustomHeaderProps {
+  navigation: NavigationProp;
+}
+
+const MainComponent: React.FC<CustomHeaderProps> = ({ navigation }) => {
   return (
     <Fragment>
-      <Navigation />
+      <Navigation navigation={navigation} />
       <FloatingActionButton />
       <HamburgerBar />
     </Fragment>
   )
 }
+
+export default MainComponent
