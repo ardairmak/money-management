e72bb8ab763f5517580906db44759aada573f1e8
Fevzi Babaoğlu <fevzibabaoglu03@gmail.com>
2024-05-25 16:24:51 +0300

    rename json attribute

--- backend/models/upcoming_payment.go
+++ backend/models/upcoming_payment.go
@@ -3,7 +3,7 @@ package models
 type UpcomingPayment struct {
 	Name          string `json:"name"`
 	Price         int    `json:"price"`
-	RenewalPeriod string `json:"renewal_period"`
+	RenewalPeriod string `json:"renewalPeriod"`
 	Repetition    int    `json:"repetition"`
 	Date          string `json:"date"`
 	Reminder      string `json:"reminder"`
